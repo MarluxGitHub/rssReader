4e3433edc16c8803ea682d392ace767034e1faaf
Marlux <marlux.td@googlemail.com>
2022-05-16 23:32:42 +0200

    Fix URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
 
-const dataProvider = simpleRestProvider('http://localhost:8080/');
+const dataProvider = simpleRestProvider('http://localhost:8080');
 
 const App = () => (
         <Admin dataProvider={dataProvider}>
